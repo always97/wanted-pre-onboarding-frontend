e2dd2d0b24d65f22d1af518b66556004e4261ac3
jw <always1997@kakao.com>
2023-09-12 15:19:54 +0900

    feat: 라우터 초기 설정 완료

---
+++ src/pages/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>홈화면입니다.</div>;
+};
+
+export default Main;
---
+++ src/pages/TodoMain.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoMain = () => {
+  return <div>투두메인</div>;
+};
+
+export default TodoMain;
