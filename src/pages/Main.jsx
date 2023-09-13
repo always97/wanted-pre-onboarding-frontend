7e4ee6f27729bcf051cb64541567b5cabf9b7a49
jw <always1997@kakao.com>
2023-09-13 10:03:16 +0900

    feat: main페이지 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,50 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "./Main.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 const Main = () => {
-  return <div>홈화면입니다.</div>;
+  const [access_token, setAccessToken] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("wanted_accessToken");
+    setAccessToken(null);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("wanted_accessToken");
+    setAccessToken(token);
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      {access_token === null ? (
+        <div>
+          <h2>로그인후 이용가능합니다.</h2>
+          <div className={styles.navBtn}>
+            <Button variant="outlined" onClick={() => navigate("/signin")}>
+              로그인
+            </Button>
+            <br />
+            <Button variant="outlined" onClick={() => navigate("/signup")}>
+              회원가입
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h2>hello wanted !!</h2>
+          <div className={styles.log}>
+            <p>로그인된 상태입니다.</p>
+            <Button variant="outlined" onClick={handleLogOut}>
+              로그아웃
+            </Button>
+          </div>
+          <Link to="/todo">Todo리스트로 이동하기</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Main;
